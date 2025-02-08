3fa855b92071ce181be9800acff6ac7dbd62e20d
Harshita Sharma D <79998309+harshitadharamansharma@users.noreply.github.com>
2025-02-08 09:11:10 +0530

    forth commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,7 +104,7 @@ const config: Config = {
           items: [
             {
               label: 'Intro - Docs',
-              to: '/docs/intro-docs',
+              to: '/docs/intro-docs/intro',
             },
           ],
         },
